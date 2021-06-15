e51204e786ea2fac9698eb64afbb3497336e256e
swan-zhao <swanzhao@gmail.com>
2021-06-15 17:01:53 -0400

    Added: cart & cocktails_in_cart Models

--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,23 @@
 //this is the access point for all things database related!
 
-const db = require("./db");
-const User = require('./models/User')
-const Cocktail = require('./models/Cocktail')
-
+const db = require('./db');
+const User = require('./models/user');
+const Cocktail = require('./models/cocktail');
+const Cart = require('./models/cart');
+const Cocktails_in_cart = require('./models/cocktails_in_cart');
 
 //associations could go here!
+Cart.belongsTo(User);
+User.hasOne(Cart);
+
+Cocktail.belongsToMany(Cart, { through: Cocktails_in_cart });
+Cart.hasMany(Cocktail);
 
 module.exports = {
   db,
   models: {
     User,
-    Cocktail
+    Cocktail,
+    Cart,
   },
-}
+};
