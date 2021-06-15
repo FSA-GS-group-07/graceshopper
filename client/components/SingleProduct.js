5bcb415c78e73ec987b4c6227a32850e553e7370
Finley Matthews <finleymatthews96@gmail.com>
2021-06-15 12:25:31 -0400

    added quantity and addtocart buttons

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -5,10 +5,28 @@ import fetchProduct from "../store/singleproduct";
 class SingleProduct extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      quantity: 1,
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleSubtract = this.handleSubtract.bind(this);
+    this.handleAddToCart = this.handleAddToCart.bind(this);
   }
 
   async componentDidMount() {
     await this.props.getProduct(this.props.match.params.id);
+  }
+
+  handleAdd() {
+    this.setState({ quantity: this.state.quantity + 1 });
+  }
+
+  handleSubtract() {
+    this.setState({ quantity: this.state.quantity - 1 });
+  }
+
+  async handleAddToCart() {
+    await this.props.addToCart(this.props.match.params.id, this.state.quantity);
   }
 
   render() {
@@ -17,9 +35,19 @@ class SingleProduct extends React.Component {
     return (
       <div>
         <h1>{product.name}</h1>
-        <h3>{product.address}</h3>
+        <h3>{product.price}</h3>
         <p>{product.description}</p>
         <img src={product.imageUrl} />
+
+        <button type="button" onClick={this.handleSubtract}>
+          -
+        </button>
+        <span>{this.state.quantity}</span>
+        <button type="button" onClick={this.handleAdd}>
+          +
+        </button>
+
+        <button type="button">Add to Cart</button>
       </div>
     );
   }
@@ -28,6 +56,7 @@ class SingleProduct extends React.Component {
 const mapDispatch = (dispatch) => {
   return {
     getProduct: (id) => dispatch(fetchProduct(id)),
+    addToCart: (id, quantity) => dispatch(thunk(id, quantity)),
   };
 };
 
