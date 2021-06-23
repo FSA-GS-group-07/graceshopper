30d255b3c8f4cee922ae9dc8ecf12e7b202e52d2
Yagna Patel <im_yagna@hotmail.com>
2021-06-23 13:13:41 -0400

    cart bug fixed

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,64 +1,15 @@
+import axios from "axios";
 import React from "react";
 import { connect } from "react-redux";
-import { fetchCart, addToCart } from "../store/cart";
-import { loadStripe } from "@stripe/stripe-js";
 import { Link } from "react-router-dom";
-import styled from 'styled-components';
-
-//CSS STYLES
-const Container = styled.div`
-  width: 75%;
-  padding: 1rem 4rem 3rem 4rem;
-  display: flex;
-  flex-direction: column;
-`;
-const List = styled.div`
-  padding: 1rem;
-  margin-left: 3rem;
-  display: flex;
-`;
-
-const LeftColumn = styled.div`
-  width: 40%;
-  position: relative;
-`;
-
-const RightColumn = styled.div`
-  width: 60%;
-  margin-left: 3rem;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const Button = styled.button`
-  background-color: transparent;
-  border: 2px solid black;
-  padding: 0.5rem 1rem 0.5rem 1rem;
-  color: black;
-  font-weight: 500;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  font-size: 1em;
-`;
-
-const LargeText = styled.span`
-  font-family: 'Bebas Neue', sans-serif;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  font-size: 3rem;
-`;
+import { fetchCart, addToCart } from "../store/cart";
 
 class Cart extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       edit: false,
     };
-    this.handleCheckout = this.handleCheckout.bind(this);
   }
 
   componentDidMount() {
@@ -72,51 +23,21 @@ class Cart extends React.Component {
   handleSubtract(cocktailId, currQty, cocktail) {
     this.props.updatedQuantity(cocktailId, -1, cocktail);
   }
-
-  async handleCheckout() {
-    const stripe = await loadStripe(
-      "pk_test_51J5C3LFT1yAmNlTZXfbIlPdca9y7GD8DILU77uUVH1AO844xp0B9UxdzJSGetlpYe4uRpRoH16hKtRyZ8aWPUeYz00RV42ERF5"
-    );
-    const response = await fetch("/api/create-checkout-session", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(this.props.cart),
-    });
-    const session = await response.json();
-    const result = await stripe.redirectToCheckout({
-      sessionId: session.id,
-    });
-    if (result.error) {
-      console.log(result.error.message);
-    }
-  }
-
   render() {
-    const cart = this.props.cart || { order: {}, cocktails: [] };
+    const { cart } = this.props;
     let total = 0;
     let subtotal = 0;
     return (
       <div className="cart">
-        <Container>
-          {cart.cocktails &&
-            cart.cocktails.map((cocktail) => (
-              <Link
-                key={cocktail.id || cocktail.cocktailId}
-                to={`/cocktails/${cocktail.id}`}
-              >
-                <List>
-                  <LeftColumn>
-                    <img src={cocktail.imageUrl} alt={cocktail.name} />
-                  </LeftColumn>
-                  <RightColumn>
-                    <LargeText>{cocktail.name}</LargeText>
-                    <h3>${cocktail.price}</h3>
-                  </RightColumn>
-                </List>
+        {cart.cocktails &&
+          cart.cocktails.map((cocktail) => (
+            <div className="cart-item" key={cocktail.id || cocktail.cocktailId}>
+              <Link to={`/cocktails/${cocktail.id}`}>
+                <span>
+                  <h1>{cocktail.name}</h1>
+                  <img src={cocktail.imageUrl} alt={cocktail.name} />
+                </span>
               </Link>
-            ))}
-          <div className="subtotal">
-            <h4>Subtotal:</h4>
 
               <h3>${cocktail.price}</h3>
 
@@ -157,49 +78,43 @@ class Cart extends React.Component {
               subtotal = Number(cocktail.price * cocktail.order_items.quantity);
               total += Number(cocktail.price * cocktail.order_items.quantity);
               return (
-                <div className="subtotal-item">
-                  <span>
-                    <h5>{cocktail.name}</h5>
-                    <h6>{cocktail.order_items.quantity}</h6>
-                    <h6>X</h6>
-                    <h6>${cocktail.price}</h6>
-                    <h6>=</h6>
-                    <h6>${subtotal}</h6>
-                  </span>
+                <div>
+                  <div className="subtotal-item">
+                    <span>
+                      (cocktail.name)
+                      <h5>{cocktail.name}</h5>
+                      <h6>{cocktail.order_items.quantity}</h6>
+                      <h6>X</h6>
+                      <h6>${cocktail.price}</h6>
+                      <h6>=</h6>
+                      <h6>${subtotal}</h6>
+                    </span>
+                  </div>
+
+                  <div className="total">
+                    <h4>Total: $ {cart.cocktails && total}</h4>
+                  </div>
+
+                  <div className="checkout">
+                    <button>Checkout</button>
+                  </div>
                 </div>
               );
             })}
         </div>
-        <div className="total">
-          <h4>Total: ${cart.cocktails && total}</h4>
-        </div>
-        {cart.cocktails && cart.cocktails.length > 0 ? (
-          <ButtonContainer className="checkout" role="link">
-            <Button onClick={this.handleCheckout}>Checkout</Button>
-          </ButtonContainer>
-        ) : (
-          <h1>
-            Oh no! Your cart is empty :/ would you like to browse a bit more?
-          </h1>
-        )}
-        </Container>
       </div>
     );
   }
 }
 
-const mapState = (state) => {
-  return {
-    cart: state.cart,
-  };
-};
+const mapState = (state) => ({
+  cart: state.cart,
+});
 
-const mapDispatch = (dispatch) => {
-  return {
-    getCart: () => dispatch(fetchCart()),
-    updatedQuantity: (cocktailId, quantity, cocktail) =>
-      dispatch(addToCart(cocktailId, quantity, cocktail)),
-  };
-};
+const mapDispatch = (dispatch) => ({
+  getCart: () => dispatch(fetchCart()),
+  updatedQuantity: (cocktailId, quantity, cocktail) =>
+    dispatch(addToCart(cocktailId, quantity, cocktail)),
+});
 
 export default connect(mapState, mapDispatch)(Cart);
