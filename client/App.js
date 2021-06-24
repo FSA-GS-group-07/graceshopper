bbe3f2a1aca48d2c0dffb32f35def5e87a0ab82c
swan-zhao <swanzhao@gmail.com>
2021-06-23 20:35:55 -0400

    more CSS changes

--- client/App.js
+++ client/App.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+import Navbar from './components/Navbar';
+import Routes from './Routes';
+import { FixedHeightContainer, Content } from './styled-components';
 
 const App = () => {
   return (
-    <div>
+    <FixedHeightContainer>
       <Navbar />
-      <Routes />
-    </div>
-  )
-}
+      <Content>
+        <Routes />
+      </Content>
+    </FixedHeightContainer>
+  );
+};
 
-export default App
+export default App;
